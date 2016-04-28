d81406b587b9ab77f64d9769d7bf5c07241af531
aroup <aroupdhruba@gmail.com>
2016-04-28 22:38:27 +0600

    Connection with socket.io

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 var PORT = process.env.PORT || 3000;
 var express = require('express');
 var app = express();
-
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
+// io is like app variable 
+// app.get ==> io.get
+
+io.on('connection',function(){
+	console.log('User connected via socket io');
+}); // 
 
 app.use(express.static(__dirname+'/public'));
 http.listen(PORT,function(){
