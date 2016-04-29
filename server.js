d232d7e6911407651ded82376b56550e197dda1f
aroup <aroupdhruba@gmail.com>
2016-04-29 10:22:49 +0600

    messages in browser UI

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ io.on('connection',function(socket){
 	console.log('User connected via socket io');
 	socket.on('message',function(message){
 		console.log('Message received'+ message.text);
-		socket.broadcast.emit('message',message);
+		io.emit('message',message);
+		//socket.broadcast.emit('message',message);
 	});
 	socket.emit('message',{
 		text: 'Welcome to chat application!'
