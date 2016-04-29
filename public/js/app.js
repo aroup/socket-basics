70114c98749d8ab7d56cdcffbb9cba4beeac6e96
aroup <aroupdhruba@gmail.com>
2016-04-29 10:24:50 +0600

    messages in browser UI

---
+++ public/js/app.js
@@ -0,0 +1,24 @@
+var socket = io();
+
+socket.on('connect',function(){
+	console.log('connected to socket io server!');
+});
+
+socket.on('message',function(message){
+	console.log('New message');
+	console.log(message.text);
+	jQuery('.messages').append('<p>' + message.text +'</p>')
+});
+
+/*
+ handles submitting of new messages 
+ */
+
+ var $form = jQuery('#message-form');
+ $form.on('submit',function(event){
+ 	event.preventDefault();
+ 	socket.emit('message',{
+ 		text: $form.find('input[name=message]').val()
+ 	});
+ 	$form.find('input[name=message]').val('');
+ });
