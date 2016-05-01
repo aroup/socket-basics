ded8745ef1fb0b0f26de0348261e898bbad2e4bf
aroup <aroupdhruba@gmail.com>
2016-05-01 11:31:23 +0600

    Timestamping in the back & front end

--- public/js/app.js
+++ public/js/app.js
@@ -1,24 +1,27 @@
 var socket = io();
 
-socket.on('connect',function(){
+socket.on('connect', function() {
 	console.log('connected to socket io server!');
 });
 
-socket.on('message',function(message){
-	console.log('New message');
+socket.on('message', function(message) {
+	var timestampMoment=moment.utc(message.timestamp);
+	console.log('New message: ');
 	console.log(message.text);
-	jQuery('.messages').append('<p>' + message.text +'</p>')
+	console.log(timestampMoment.local().format('h:mm a'));
+
+	jQuery('.messages').append('<p><strong>'+timestampMoment.local().format('h:mm a')+': </strong>' + message.text + '</p>')
 });
 
 /*
  handles submitting of new messages 
  */
 
- var $form = jQuery('#message-form');
- $form.on('submit',function(event){
- 	event.preventDefault();
- 	socket.emit('message',{
- 		text: $form.find('input[name=message]').val()
- 	});
- 	$form.find('input[name=message]').val('');
- });
+var $form = jQuery('#message-form');
+$form.on('submit', function(event) {
+	event.preventDefault();
+	socket.emit('message', {
+		text: $form.find('input[name=message]').val()
+	});
+	$form.find('input[name=message]').val('');
+});
--- server.js
+++ server.js
@@ -3,18 +3,23 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var moment = require('moment');
 // io is like app variable 
 // app.get ==> io.get
 
 io.on('connection',function(socket){
+	now=moment();
 	console.log('User connected via socket io');
 	socket.on('message',function(message){
-		console.log('Message received'+ message.text);
+		console.log('Message received: '+ message.text);
+		message.timestamp=now.valueOf();
 		io.emit('message',message);
 		//socket.broadcast.emit('message',message);
 	});
+	//console.log(typeof (now.valueOf()));
 	socket.emit('message',{
-		text: 'Welcome to chat application!'
+		text: 'Welcome to chat application!',
+		timestamp: now.valueOf()
 	});
 }); // 
 
